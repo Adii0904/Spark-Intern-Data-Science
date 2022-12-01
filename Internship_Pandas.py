11f639666804c681d5298176eeb5372ff9b3a119
Aditya Prakash <96534371+Adii0904@users.noreply.github.com>
2022-12-01 22:26:42 +0530

    Create Internship_Pandas.py
    First Task

---
+++ Internship_Pandas.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+dict = {
+    "Students_Name":['Aditya','Aarohi','Gunja','Rashmi','Guncha'],
+    "Study_Hour":[1,3,5,6,7],
+    "Percentage":[22,45,67,89,90]
+}
+
+
+# In[5]:
+
+
+df=pd.DataFrame(dict)
+
+
+# In[6]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
